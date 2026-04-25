dfde784812c37a9e33c5945a96196143c049a1ed
Reyad-Hoq <rafiulhoq40@gmail.com>
2026-04-25 21:17:58 +0600

    timeline and fontfamily added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,21 +2,20 @@ import footerImg from "../../assets/logo-xl.png"
 import { RiInstagramFill } from "react-icons/ri";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+
 const Footer = () => {
   return (
     <div className="bg-[#244D3F]  p-10">
       <footer className="footer footer-horizontal footer-center text-primary-content w-9/12 mx-auto">
         <aside>
           <img src={footerImg} alt="" />
-          <p className="font-bold">
-            ACME Industries Ltd.
-            <br />
-            Providing reliable tech since 1992
+          <p >
+            Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.
           </p>
-          <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+
         </aside>
         <nav>
-          <h6 className="text-white text-lg">Social Links</h6>
+          <h6 className="text-white text-lg font-semibold">Social Links</h6>
           <div className="grid grid-flow-col gap-4 text-[#101727]">
             <div className="bg-white w-7 h-7 flex items-center justify-center rounded-full">
               <a>
@@ -38,24 +37,25 @@ const Footer = () => {
 
           </div>
         </nav>
-      </footer>
-      <footer className="flex flex-col-reverse md:flex-row justify-between items-center gap-10 text-gray-400 text-sm border-gray-500 border-t p-10">
-        <aside className="grid-flow-col items-center">
-          <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
-        </aside>
+        <footer className="flex flex-col-reverse md:flex-row justify-between items-center gap-10 text-gray-400 text-sm border-gray-500 border-t p-10">
+          <aside className="grid-flow-col items-center">
+            <p>© {new Date().getFullYear()} KeenKeeper All right reserved</p>
+          </aside>
 
-        <div className="grid grid-flow-col gap-4">
-          <a>
-            Privacy Policy
-          </a>
-          <a>
-            Terms of Service
-          </a>
-          <a>
-            Cookies
-          </a>
-        </div>
+          <div className="grid grid-flow-col gap-4">
+            <a>
+              Privacy Policy
+            </a>
+            <a>
+              Terms of Service
+            </a>
+            <a>
+              Cookies
+            </a>
+          </div>
+        </footer>
       </footer>
+
     </div>
   );
 };
